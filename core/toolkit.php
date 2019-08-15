c1ef83da80cc272c7e70942268c6b8625304d5ad
h3rb <hgilliland@gmail.com>
2019-08-15 09:32:15 -0400

    Removing DS constant

--- core/toolkit.php
+++ core/toolkit.php
@@ -1,14 +1,17 @@
 <?php
-	// note: this toolkit is extremely based on kirby toolkit
-	// http://toolkit.getkirby.com/
 
-	// helper functions
-	require_once('toolkit' . DS . 'helpers.php');
+ $DS = DIRECTORY_SEPARATOR;
 
-	// classes
-	require_once('toolkit' . DS . 'g.php');
-	require_once('toolkit' . DS . 'path.php');
-	require_once('toolkit' . DS . 'dir.php');
-	require_once('toolkit' . DS . 'file.php');
-	require_once('toolkit' . DS . 'parser.php');
+        // note: this toolkit is extremely based on kirby toolkit
+        // http://toolkit.getkirby.com/
+
+        // helper functions
+        require_once('toolkit' . $DS . 'helpers.php');
+
+        // classes
+        require_once('toolkit' . $DS . 'g.php');
+        require_once('toolkit' . $DS . 'path.php');
+        require_once('toolkit' . $DS . 'dir.php');
+        require_once('toolkit' . $DS . 'file.php');
+        require_once('toolkit' . $DS . 'parser.php');
 ?>
